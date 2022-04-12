84313dd14184802a73128ad8d9eeb266adc88de6
ahnyh27 <dilemma90@naver.com>
2022-04-12 18:27:54 +0900

    page 346

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,8 +1,54 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useRef, useState, useEffect} from 'react';
+import {Animated, Button, StyleSheet, View} from 'react-native';
+
+function SlideLeftAndRight() {
+  const animation = useRef(new Animated.Value(0)).current;
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: enabled ? 1 : 0,
+      useNativeDriver: true,
+    }).start();
+  }, [enabled, animation]);
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled);
+        }}
+      />
+    </View>
+  );
+}
 
 function CalendarScreen() {
-  return <View style={styles.block}></View>;
+  return (
+    <View style={styles.block}>
+      <SlideLeftAndRight />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -10,6 +56,11 @@ const styles = StyleSheet.create({
   text: {
     padding: 16,
     fontSize: 24,
+  },
+  rectangle: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#000',
   },
 });
 
